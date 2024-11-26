a2ccc074ee4a214b5b76d58eec01bb6134ce2637
Johnatan Ramirez <19001825@galileo.edu>
2024-11-26 13:34:06 -0600

    Repositorio base - Mini-Proyecto-2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Elaboración de proyecto 2.',
+    author='Johnatan Ramirez',
+    license='',
+)
